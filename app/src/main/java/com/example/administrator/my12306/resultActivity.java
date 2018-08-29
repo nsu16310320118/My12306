b0bb37669a7ba255d955d226c0c832cd6797a23b
nsu16310320118 <540998359@qq.com>
2018-08-29 11:14:23 +0800

    version14

---
+++ app/src/main/java/com/example/administrator/my12306/resultActivity.java
@@ -0,0 +1,16 @@
+package com.example.administrator.my12306;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class resultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
+    public void setTitle(CharSequence title){
+//        (findViewById(R.id.text_title)).setText(title);
+    }
+}
