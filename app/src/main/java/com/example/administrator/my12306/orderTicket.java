a64e37b31280ff8ad97707277f501d07ad183809
nsu16310320118 <540998359@qq.com>
2018-08-30 16:36:29 +0800

    version17

--- app/src/main/java/com/example/administrator/my12306/orderTicket.java
+++ app/src/main/java/com/example/administrator/my12306/orderTicket.java
@@ -3,14 +3,12 @@ package com.example.administrator.my12306;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class resultActivity extends AppCompatActivity {
-
+public class orderTicket extends AppCompatActivity {
+private TopBar topBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_result);
-    }
-    public void setTitle(CharSequence title){
-//        findViewById(R.id.text_title).setText(title);
+        setContentView(R.layout.activity_order_ticket);
+        topBar=findViewById(R.id.topbar);
     }
 }
